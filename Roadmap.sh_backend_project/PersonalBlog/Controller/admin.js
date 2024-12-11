1722600c8dfd5b25c7d8855d80e82986bcacb786
Mr-mahato <cmhandan@gmail.com>
2024-12-11 21:48:43 +0530

    added few utility functionality and endpoint for the admin

--- Roadmap.sh_backend_project/PersonalBlog/Controller/admin.js
+++ Roadmap.sh_backend_project/PersonalBlog/Controller/admin.js
@@ -1,19 +1,81 @@
-const getPersonalBlog = (req, res) => {
-  //  well i am fetching
-  res.status(200).json({
-    message: "These are the blog which is to be send to the end user",
-  });
+const blogModel = require("../Model/Blog");
+
+const getPersonalBlog = async (req, res) => {
+  try {
+    const blogCollection = await blogModel.find({});
+    res.status(200).json({
+      status: 1,
+      message: "Blog fetched",
+      blog: blogCollection,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      status: 0,
+      message: "Blog fetching error 404",
+    });
+  }
 };
 
-const addPersonalBlog = async(req,res)=>{
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+const addPersonalBlog = async (req, res) => {
+  try {
+    const newBlog = req.body;
+    const nb = await blogModel(newBlog);
+    await nb.save();
+    console.log(nb);
+    console.log(newBlog);
+    res.status(200).json({
+      message: "blog created successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      message: "Error occur",
+    });
+  }
+};
+
+const deleteParticularBlog = async (req, res) => {
+  try {
+    const { blogId } = req.params;
+    const deletedBlog = await blogModel.findByIdAndDelete(blogId);
+    res.status(200).json({
+      message: "blog deleted successfully",
+      blogId: deletedBlog._id,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      status: 0,
+      message: "blog not found",
+    });
+  }
+};
+
+const editParticularBlog = async (req, res) => {
+  try {
+    const { blogId } = req.params;
+    const updatedBlog = await blogModel.findByIdAndUpdate(blogId, req.body, {
+      new: true,
+    });
+    console.log(updatedBlog);
+    res.status(200).json({
+      message: "blog updated successfully",
+      blog: updatedBlog,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(404).json({
+      status: 0,
+      message: "blog not found",
+    });
+  }
+};
 
 module.exports = {
   getPersonalBlog,
-  addPersonalBlog
+  addPersonalBlog,
+  deleteParticularBlog,
+  editParticularBlog,
 };
