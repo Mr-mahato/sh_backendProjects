ff5e16768531bebf093eda21f1b5bd2c55fc841f
Mr-mahato <cmhandan@gmail.com>
2024-12-18 23:00:50 +0530

    updated the backend routes

--- PersonalBlog/backend/Routes/AdminRoutes.js
+++ PersonalBlog/backend/Routes/AdminRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {  addPersonalBlog, deleteParticularBlog, editParticularBlog } = require("../Controller/admin");
+const {  addPersonalBlog, deleteParticularBlog, getAllBlog ,  editParticularBlog } = require("../Controller/admin");
 const authMiddleware = require("../Middleware/authMiddleware");
 
 const adminRouter = express.Router();
@@ -7,8 +7,10 @@ const adminRouter = express.Router();
 adminRouter.use(authMiddleware);
 
 adminRouter.post("/addblog", addPersonalBlog);
+adminRouter.get("/getAllBlog/:userId",getAllBlog);
 adminRouter.delete("/deleteBlog/:blogId",deleteParticularBlog);
 adminRouter.patch("/editBlog/:blogId",editParticularBlog);
+
 module.exports = {
   adminRouter,
 };
